79804892e12b40c4d146a62f0261c1961aabfbc1
U1 <U1@comp2112>
2020-04-23 17:43:03 +0300

    test

---
+++ blink_1/blink_1.cpp
@@ -0,0 +1,20 @@
+#include <Arduino.h>
+
+// PCF8591T
+#define I2C_DTA	20
+#define I2C_SCK	21
+//
+#define MLED 13
+
+
+
+void setup() {
+  pinMode(MLED,OUTPUT);
+}
+
+void loop() {
+	digitalWrite(MLED,LOW);
+    delay (1000);
+    digitalWrite(MLED,HIGH);
+    delay (1000);
+}
